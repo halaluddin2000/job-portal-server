2cb779f17f0b2683d885e79c266717523f5feea4
halaluddin2000 <helaluddin7001@gmail.com>
2025-04-27 00:47:00 +0600

    jobs get or spasific id find one

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middle wore
 app.use(cors())
@@ -38,6 +38,14 @@ async function run() {
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.findOne(query)
+            res.send(result)
+
         })
 
     } finally {
